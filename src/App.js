97e4df49712066b410b917c66e871a5eb96a72d1
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-07-15 15:40:54 -0600

    created components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header >
+      <nav className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>ReactsFacts</p>
+      </nav>
+    </header>
   );
 }
 
-export default App;
+function MainContent() {
+  return (
+    <div className="App-main">
+    <h1>Fun facts about React</h1>
+    <ul className="App-list">
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+  )
+}
+
+function Footer () {
+  return (
+    <footer className="App-footer">
+      <p>2022 Maryam Khan</p>
+    </footer>
+  )
+}
+
+function Page() {
+  return (
+    <div className="App-page">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+export default Page;
